c1274bab102982f2ee4442ffd97f02e9a8cd7a08
Richard <digitalsadhu@gmail.com>
2021-08-02 15:38:00 +0200

    refactor: extract verify-conditions into own file

--- index.js
+++ index.js
@@ -1,46 +1,21 @@
 const { join } = require("path");
-const AggregateError = require("aggregate-error");
 const fetch = require("node-fetch");
 const eik = require("@eik/cli");
 const common = require("@eik/common");
 const json = require("@eik/cli/utils/json");
+const verifyConditions = require("./lib/verify-conditions");
 
-let eikToken = "";
-let eikjson = {};
-let versionToPublish;
-let publishNeeded;
+class State {
+  eikToken = "";
+  eikJSON = {};
+  versionToPublish = null;
+  publishNeeded = null;
+}
 
-module.exports.verifyConditions = async function verifyConditions(
-  options,
-  context
-) {
-  const { env, cwd } = context;
-  const errors = [];
+const state = new State();
 
-  if (!env.EIK_TOKEN)
-    errors.push(new Error("EIK_TOKEN environment variable not found"));
-
-  if (!["major", "minor", "patch"].includes(options.level || "patch")) {
-    errors.push(
-      new Error(
-        'plugin option "level" must be one of "major", "minor" or "patch"'
-      )
-    );
-  }
-
-  try {
-    eikjson = common.helpers.configStore.findInDirectory(cwd);
-  } catch (err) {
-    errors.push(err);
-  }
-
-  eikToken = await eik.login({ server: eikjson.server, key: env.EIK_TOKEN });
-  if (!eikToken) errors.push(new Error("Unable to log in to Eik server"));
-
-  if (errors.length > 0) {
-    throw new AggregateError(errors);
-  }
-};
+module.exports.verifyConditions = (options, context) =>
+  verifyConditions(options, context, state);
 
 module.exports.analyzeCommits = async function analyzeCommits(
   options,
@@ -55,10 +30,10 @@ module.exports.analyzeCommits = async function analyzeCommits(
     files,
     map,
     out,
-  } = eikjson;
+  } = state.eikJSON;
 
   try {
-    versionToPublish = await eik.version({
+    state.versionToPublish = await eik.version({
       name,
       version: currentVersion,
       type,
@@ -70,9 +45,9 @@ module.exports.analyzeCommits = async function analyzeCommits(
       out,
     });
     logger.log(
-      `Eik version updated from v${currentVersion} to v${versionToPublish}.`
+      `Eik version updated from v${currentVersion} to v${state.versionToPublish}.`
     );
-    publishNeeded = true;
+    state.publishNeeded = true;
     return options.level || "patch";
   } catch (err) {
     logger.log(err.message);
@@ -85,15 +60,15 @@ module.exports.analyzeCommits = async function analyzeCommits(
   url.searchParams.set("ts", Date.now());
   const result = await fetch(url);
   if (result.ok) {
-    publishNeeded = false;
+    state.publishNeeded = false;
     logger.log(
       `The current version of this package is already published. Publishing is not needed.`
     );
     return null;
   }
 
-  publishNeeded = true;
-  versionToPublish = currentVersion;
+  state.publishNeeded = true;
+  state.versionToPublish = currentVersion;
   logger.log(
     `Current Eik version not yet published. Versioning change is not needed.`
   );
@@ -101,20 +76,20 @@ module.exports.analyzeCommits = async function analyzeCommits(
 };
 
 module.exports.verifyRelease = async function verifyRelease(options, context) {
-  if (!publishNeeded) return;
+  if (!state.publishNeeded) return;
   const { cwd, logger } = context;
 
   try {
-    const { server, name, files, type, map, out } = eikjson;
+    const { server, name, files, type, map, out } = state.eikJSON;
     const result = await eik.publish({
       name,
       type,
       server,
       files,
       cwd,
-      token: eikToken,
+      token: state.eikToken,
       dryRun: true,
-      version: versionToPublish,
+      version: state.versionToPublish,
       map,
       out,
     });
@@ -135,40 +110,40 @@ module.exports.verifyRelease = async function verifyRelease(options, context) {
 };
 
 module.exports.generateNotes = async function generateNotes() {
-  if (!publishNeeded) return;
-  return `New Eik package version ${versionToPublish} published.`;
+  if (!state.publishNeeded) return;
+  return `New Eik package version ${state.versionToPublish} published.`;
 };
 
 module.exports.prepare = async function prepare(options, context) {
-  if (!publishNeeded) return;
+  if (!state.publishNeeded) return;
   const { cwd, logger } = context;
-  if (eikjson.version !== versionToPublish) {
-    logger.log(`Version ${versionToPublish} written to eik.json`);
-    await json.writeEik({ version: versionToPublish }, { cwd });
+  if (state.eikJSON.version !== state.versionToPublish) {
+    logger.log(`Version ${state.versionToPublish} written to eik.json`);
+    await json.writeEik({ version: state.versionToPublish }, { cwd });
   }
 };
 
 module.exports.publish = async function publish(options, context) {
-  if (!publishNeeded) return;
+  if (!state.publishNeeded) return;
   const { cwd, logger } = context;
   try {
-    const { server, name, files, type, map, out } = eikjson;
+    const { server, name, files, type, map, out } = state.eikJSON;
     const result = await eik.publish({
       name,
       type,
       server,
       files,
       cwd,
-      token: eikToken,
+      token: state.eikToken,
       dryRun: false,
-      version: versionToPublish,
+      version: state.versionToPublish,
       map,
       out,
     });
 
     const filesPublished = result.files.filter((file) => file.type === "pkg");
     logger.log(
-      `Published ${filesPublished.length} file(s) to Eik server ${eikjson.server}`
+      `Published ${filesPublished.length} file(s) to Eik server ${state.eikJSON.server}`
     );
   } catch (err) {
     logger.log(
@@ -179,22 +154,22 @@ module.exports.publish = async function publish(options, context) {
 };
 
 module.exports.success = async function success(options, context) {
-  if (!publishNeeded) return;
+  if (!state.publishNeeded) return;
   const { logger } = context;
-  const { server, name, type } = eikjson;
+  const { server, name, type } = state.eikJSON;
   const url = new URL(
-    join(common.helpers.typeSlug(type), name, versionToPublish),
+    join(common.helpers.typeSlug(type), name, state.versionToPublish),
     server
   );
   url.searchParams.set("ts", Date.now());
   const result = await fetch(url);
   if (result.ok) {
     logger.log(
-      `Successfully published package ${name} (v${versionToPublish}) to ${url.href}.`
+      `Successfully published package ${name} (v${state.versionToPublish}) to ${url.href}.`
     );
   } else {
     logger.log(
-      `Unable to locate package ${name} (v${versionToPublish}) at ${url.href}. It appears something went wrong during publish.`
+      `Unable to locate package ${name} (v${state.versionToPublish}) at ${url.href}. It appears something went wrong during publish.`
     );
   }
 };
