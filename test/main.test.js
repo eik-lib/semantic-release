153e25d3e3bb6484b6b08b2061fee96db1da3c9f
William Killerud <william.killerud@schibsted.com>
2024-08-09 12:42:49 +0200

    fix: include type definitions

--- test/main.test.js
+++ test/main.test.js
@@ -1,13 +1,13 @@
-import tap from 'tap';
-import plugin from '../lib/main.js';
+import tap from "tap";
+import plugin from "../lib/main.js";
 
-tap.test('Simple test', (t) => {
-    t.type(plugin.verifyConditions, 'function');
-    t.type(plugin.analyzeCommits, 'function');
-    t.type(plugin.verifyRelease, 'function');
-    t.type(plugin.generateNotes, 'function');
-    t.type(plugin.prepare, 'function');
-    t.type(plugin.publish, 'function');
-    t.type(plugin.success, 'function');
-    t.end();
+tap.test("Simple test", (t) => {
+	t.type(plugin.verifyConditions, "function");
+	t.type(plugin.analyzeCommits, "function");
+	t.type(plugin.verifyRelease, "function");
+	t.type(plugin.generateNotes, "function");
+	t.type(plugin.prepare, "function");
+	t.type(plugin.publish, "function");
+	t.type(plugin.success, "function");
+	t.end();
 });
